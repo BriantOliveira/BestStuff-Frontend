820869524db66b9695f7b157c96540b2d1ccb154
Tassos <lambrou@gmail.com>
2017-11-05 15:43:18 -0800

    Stopped cards from hopping on the show view

--- src/components/Contests/Contest.js
+++ src/components/Contests/Contest.js
@@ -9,7 +9,7 @@ const PLACES = "AIzaSyCh4He2DUJ9cCgC6kl31vAmpSH2cqGq0r4";
 class ItemCard extends Component {
   render() {
     return(
-      <div className="card" style={{paddingLeft:40, paddingBottom:20}}>
+      <div className="card-plain" style={{paddingLeft:40, paddingBottom:20}}>
         <h2 className="card-title"> {this.props.name} </h2>
         <p> {this.props.loc} </p>
       </div>
@@ -26,6 +26,7 @@ export default class Contest extends Component{
   }
 
   componentWillMount() {
+
   }
 
   findContestById(contests, id) {
@@ -52,7 +53,7 @@ export default class Contest extends Component{
         <div className="row">
           <div className="col-md-6">
             <div className="container">
-              <h2 className="title">{contest.name}</h2>
+              <h1 className="title">{contest.name}</h1>
               <a href="/items/new" className="btn btn-primary btn-lg active" role="button"><i className="nc-icon nc-simple-add"></i> Enroll/Add New Item to this Contest</a>
 
               {this.drawContests(contest.items)}
