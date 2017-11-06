5af34a8a7e77a4957a4952e3d5c35c759100c341
Tassos <lambrou@gmail.com>
2017-11-05 16:15:29 -0800

    Styling for Item New form and some other minor changes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -11,33 +11,39 @@ class Home extends Component{
   }
 
   componentWillMount() {
-{ /*
-    fetch('https://localhost:8000/contests').then((res)=>{
+    { /*
+      fetch('https://localhost:8000/contests').then((res)=>{
       return res.text();
-    }).then((text)=>{
+      }).then((text)=>{
       console.log(text);
-    }).catch((err)=>{
+      }).catch((err)=>{
       console.log(err.message);
-    })
-*/}
-  }
+      })
+      */}
+    }
 
-  getAllContests() {
-    // TODO: replace local test variable w/ fetch call to backend
-    return testData.contests.map((contest, index) => {
-      console.log()
-      return <ContestCard key={index} name={contest.name} id={contest.id}/>
-    })
-  }
+    getAllContests() {
+      // TODO: replace local test variable w/ fetch call to backend
+      return testData.contests.map((contest, index) => {
+        console.log()
+        return <ContestCard key={index} name={contest.name} id={contest.id}/>
+      })
+    }
 
-  render() {
-    return (
-      <div className={styles.content}>
-        <h1 className="page-title">BestStuff</h1>
-        {this.getAllContests()}
-      </div>
-    );
-  }
-};
+    render() {
+      return (
+        <div className={styles.content}>
+          <div className="container">
+            <div className="row">
+              <div className="col-md-6 offset-md-3">
+                <h1 className="page-title">BestStuff</h1>
+                {this.getAllContests()}
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    }
+  };
 
-export default Home
+  export default Home
--- src/components/Items/ItemNew.js
+++ src/components/Items/ItemNew.js
@@ -9,28 +9,44 @@ export default class ItemNew extends Component{
 
   render() {
     return (
-      <div className="section">
-        <div className="container">
-          <div className="col-md-4">
-            <form action="/items/create" >
-              <div class="form-group">
-                <input name="name" type="text" class="form-control" placeholder="Item Name"/>
-              </div>
-              <div class="form-group">
-                <input name="place_name" type="text" class="form-control" placeholder="Place Name"/>
-              </div>
-              <div class="form-group">
-                <input name="lat" type="text" class="form-control" placeholder="Latitude"/>
-              </div>
-              <div class="form-group">
-                <input name="long" type="text" class="form-control" placeholder="Longitude"/>
-              </div>
-              <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
-          </div>
 
+      <div className="contactus-1" >
+        <div className="container">
+          <div className="row">
+            <div className="col-md-10 ml-auto mr-auto">
+              <div className="card card-plain card-contact no-transition">
+                <h3 className="card-title">Add New Item!</h3>
+                <div className="row">
+
+                  <div className="col-md-5 mr-auto">
+                    <form role="form" action="http://localhost:8000/items/create" method="post">
+                      <div className="form-group label-floating">
+                        <label className="control-label">Item Name</label>
+                        <input name="name" type="text" className="form-control" placeholder="Name"/>
+                      </div>
+                      <div className="form-group">
+                        <label className="control-label">Place Name</label>
+                        <input name="place_name" type="text" className="form-control" placeholder="Place Name"/>
+                      </div>
+                      <div className="form-group">
+                        <label className="control-label">Latitude</label>
+                        <input name="lat" type="number" className="form-control" placeholder="Latitude"/>
+                      </div>
+                      <div className="form-group label-floating">
+                        <label className="control-label">Longitude</label>
+                        <input name="long" type="number" className="form-control" placeholder="Longitude"/>
+                      </div>
+
+                      <button type="submit" className="btn btn-primary">Submit</button>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
+
     );
   }
 };
