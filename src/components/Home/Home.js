705629cd596991ddf6bc94545c57d11353ff14c9
Tassos <lambrou@gmail.com>
2017-11-05 19:13:08 -0800

    updated Contest create form in Home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,60 +22,53 @@ class Home extends Component{
       */}
     }
 
-  getAllContests() {
-    // TODO: replace local test variable w/ fetch call to backend
+    getAllContests() {
+      // TODO: replace local test variable w/ fetch call to backend
 
-    fetch('https://localhost:8000/contests').then((res) => {
-      console.log(res);
-      return res.json();
-    }).then((text) => {
-      console.log(text);
-      return testData.contests.map((contest, index) => {
-        return <ContestCard key={index} name={contest.name} id={contest.id}/>
-      }).catch((err) => {
-        console.error(err);
-      })
-    });
-  }
+      fetch('https://localhost:8000/contests').then((res) => {
+        console.log(res);
+        return res.json();
+      }).then((text) => {
+        console.log(text);
+        return testData.contests.map((contest, index) => {
+          return <ContestCard key={index} name={contest.name} id={contest.id}/>
+        }).catch((err) => {
+          console.error(err);
+        })
+      });
+    }
 
-  render() {
-    return (
-      <div className={"main"}>
-      <div className={"container"} >
-        <h1 style={{padding:20}}> Best Stuff </h1>
-        <div className={"row"}>
-          <div className={"col-md-7 col-sm-7"}>
-            <div className={"container"}>
-              {this.getAllContests()}
-            </div>
-          </div>
-          <div className={"col-md-5 col-sm-5"}>
-            <div className={"container"}>
-              <form>
-                <div className={"form-group"}>
-                  <h6> Name </h6>
-                  <input className={"form-control border-input"} placeholder="Enter the item name"/>
+    render() {
+      return (
+        <div className={"main"}>
+          <div className={"container"} >
+            <h1 style={{padding:20}}> Best Stuff </h1>
+            <div className={"row"}>
+              <div className={"col-md-7 col-sm-7"}>
+                <div className={"container"}>
+                  {this.getAllContests()}
                 </div>
-                <div className={"form-group"}>
-                  <h6> Location Name </h6>
-                  <input className={"form-control border-input"} placeholder="Search for a restaurant"/>
+              </div>
+              <div className="col-md-5 col-sm-5">
+                <div className="container">
+                  <div className="card card-plain">
+                    <h3>Add a new contest...</h3>
+                    <form role="form" action="http://localhost:8000/contests/create" method="post">
+                      <div className="form-group">
+                        <h6> Name </h6>
+                        <input name="name" className="form-control border-input" placeholder="Enter the item name"/>
+                      </div>
+                      <button type="submit" className="btn btn-primary">Submit</button>
+                    </form>
+
+                  </div>
                 </div>
-                <div className={"form-group"}>
-                  <h6> Latitude </h6>
-                  <input className={"form-control border-input"} placeholder="Enter Longitude (temporary)"/>
-                </div>
-                <div className={"form-group"}>
-                  <h6> Longitude </h6>
-                  <input className={"form-control border-input"} placeholder="Enter Longitude (temporary)"/>
-                </div>
-              </form>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      </div>
-    );
-  }
-};
+      );
+    }
+  };
 
   export default Home
