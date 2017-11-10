6bf8beed9b4b52f90c74867cc16645282ead3fb5
Tassos <lambrou@gmail.com>
2017-11-10 14:34:58 -0800

    Resolved Conflict on Map Keys

--- src/components/Misc/MapContainer.js
+++ src/components/Misc/MapContainer.js
@@ -1,11 +1,6 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
 import { Map, InfoWindow, GoogleApiWrapper } from 'google-maps-react';
 const MAPS = "AIzaSyAD0CS7T_DtdpNuqcin9DtX79brSndlI6U";
-=======
-import { Map, InfoWindow, GoogleApiWrapper, Marker } from 'google-maps-react';
-export const MAPS = "AIzaSyBBYy-u-ZsF-3krZWO2fpqp2LYp2noQRbs";
->>>>>>> 7de4ff09bf07ef614b1107ddd9656ca4ff9f0cfd
 
 export class MapContainer extends Component {
   constructor(props) {
